7e671022c89e8bb3a72506daf9c95329a667209c
Steven <xing31120@163.com>
2024-05-27 01:26:51 +0800

    改造

--- application/pc/controller/Teacher.php
+++ application/pc/controller/Teacher.php
@@ -28,10 +28,17 @@ class Teacher extends Base {
         $userInfo = (new HbhUsers())->info($uid);
         $userInfo['expiry_date_en'] = date("d M,Y");
 
-        $day = input('day', '');
-        $day = $day ?: date('Y-m-d');
-
-        $this->assign('day', $day);
+        //查询当天前后14天, 总共28天的分组统计数据
+        $time = time();
+        $model= new HbhBookCourse();
+        $start_time = date("Y-m-d", $time - 86400 * 15);
+        $end_time = date("Y-m-d", $time + 86400 * 15);
+        $where[] = ['day', '>', $start_time];
+        $where[] = ['day', '<', $end_time];
+        $where[] = ['teacher_uid', '=', $uid];
+        $day_group_count = $model->where($where)->group("day")->having('transNum > 0')->order('day asc')->field("day, count(*) as transNum")->select()->toArray();
+        $day_group_count = array_column($day_group_count, 'transNum', 'day');
+        $this->assign('day_group_count', $day_group_count);
         $this->assign('userInfo', $userInfo);
         return $this->fetch();
     }
