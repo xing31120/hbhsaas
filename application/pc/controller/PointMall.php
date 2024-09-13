54eda1080f9d6d1e58ca236d960da68b1d9df006
Steven <xing31120@163.com>
2024-09-13 22:46:48 +0800

    权限管理 没有操作按钮问题修复, 从只能uid=1 修改成 uid <=5

---
+++ application/pc/controller/PointMall.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\pc\controller;
+
+use app\common\model\HbhPageConfig;
+use app\common\model\HbhUsers;
+
+class PointMall extends Base {
+
+    function main() {
+        echo 112233;
+        return $this->fetch();
+    }
+
+    function detail() {
+
+        return $this->fetch();
+    }
+}
