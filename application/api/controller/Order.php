97fa7059d1418318fd31bf42346707c82459566a
Steven <xing31120@163.com>
2024-09-17 23:41:11 +0800

    1

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -38,7 +38,7 @@ class Order extends Base{
         $data = input();
         Hook::listen('app_init', $data);
 
-//$row = HbhNotifyLog::get(68)->toArray();
+//$row = HbhNotifyLog::get(80)->toArray();
 //$data = json_decode($row['notify_data'], true);
 //echo json_encode($data);exit;
 
